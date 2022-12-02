123bef9e47538f3f7505de8f1e35b5b1a0cd334a
sajjad-mirzapour <sajjad.mirzapour772gmail.com>
2022-12-02 10:11:18 +0330

    feat(hooks):add get favorite songs

---
+++ src/hooks/useGetFavoriteMusic.js
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'src/utils/axios';
+
+export function useGetFavoriteMusic(enabled = true) {
+    return useQuery(
+        {
+            queryKey: ['favoriteMusic'],
+            queryFn: () => axios.get('/music/favorite').then(({ data }) => data),
+            enabled,
+            initialData: [],
+            onError: (error) => {
+                console.log(error);
+            },
+        }
+    )
+}
